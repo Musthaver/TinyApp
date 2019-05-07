2093e4e01f8ae2a7e0429889bd2215174ea8e417
Robin Westhaver <robinwesthaver@gmail.com>
2019-05-07 21:42:37 +0000

    added body-parser and POST

--- express_server.js
+++ express_server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -20,6 +23,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 app.get("/urls", (req, res) => {
  let templateVars = { urls: urlDatabase };
